555ae0b9f14d968d9152cb242ee63511b51465fb
fareedjava <124156593+fareedjava@users.noreply.github.com>
2023-02-09 23:31:17 +0530

    Add files via upload

---
+++ Farooq.java
@@ -0,0 +1,14 @@
+
+public class Farooq {
+
+	public Farooq() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Aut
+		System.out.println("hello");
+
+	}
+
+}
